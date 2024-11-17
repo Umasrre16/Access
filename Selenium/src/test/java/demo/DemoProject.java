e82c505f75f70b85d05096f34f16259cf33e866c
us <us@Uma>
2024-11-17 19:07:14 +0530

    First Push

---
+++ Selenium/src/test/java/demo/DemoProject.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoProject {
+	@Test
+	public void demo() {
+		Reporter.log("Project Updated");	
+	}
+}
